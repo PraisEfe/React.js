7245f38df29545c66908426435233e7a4417b97e
PraisEFe <efeorogun07@gmail.com>
2024-04-17 11:52:47 +0100

    first commit

--- CPLUS.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-using namespace std;
-int main()
-{
-	int num1 = 15, num2 = 10, num3 =20, sum;
-	sum = num1 + num2 + num3;
-	cout<< "sum of "<<num1<<" and "<<num2<<" and "<<num3<<" is "<<sum;
-	return 0;
-}
